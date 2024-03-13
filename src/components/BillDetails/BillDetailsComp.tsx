71e5a7ca7dcf042962dc3b912c765952250c0906
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-03-13 14:20:11 +0530

    Tax Charges Api fix

--- src/components/BillDetails/BillDetailsComp.tsx
+++ src/components/BillDetails/BillDetailsComp.tsx
@@ -1,37 +1,39 @@
 import React from 'react'
 
-export const BillDetailsComp = () => {
+interface BillDetailsProps {
+  totalPrice: number;
+  totalTax: number;
+  taxList: { key: string; value: any; }[];
+}
+
+export const BillDetailsComp: React.FC<BillDetailsProps> = ({ totalPrice, totalTax, taxList }) => {
   return (
-      <div className="bg-white mx-5 mb-4 h-60 px-0 pt-3 items-center  rounded-md shadow-lg text-blue-dark">
-      <div className="flex flex-col">
-      <h4 className="mx-3 font-semibold text-green-mineral text-xs">Bill Details</h4>
-      <hr className="bg-silver  mx-3 my-3" />
-      <div className="flex flex-row justify-between">
-      <h5 className="ml-3.5 font-semibold text-xs">Item Total :</h5>
-      <h5 className="mx-10 font-semibold text-sm"><span>&#8377;</span>150.00</h5>
-      </div>
-      <hr className="bg-silver  mx-3 my-3" />
-      <div className="flex flex-col">
-      <div className="flex flex-row justify-between">
-      <h5 className="mx-3.5 font-normal text-xs">Service Charge :</h5>
-      <h5 className="mx-10 font-normal font-semibold text-grey text-xs"><span>&#8377;</span>30.00</h5>
-      </div>
-      <div className="flex flex-row py-2.5 justify-between">
-      <h5 className="mx-3.5 font-normal text-xs">CGST :</h5>
-      <h5 className="mx-10 font-semibold text-grey text-xs"><span>&#8377;</span>30.00</h5>
-      </div>
-      <div className="flex flex-row justify-between">
-      <h5 className="mx-3.5 font-normal text-xs">SGST :</h5>
-      <h5 className="mx-10  font-semibold text-grey text-xs"><span>&#8377;</span>30.00</h5>
-      </div>
-      </div>
-      <hr className="bg-silver  mx-3 my-3" />
-      <div className="flex flex-row justify-between">
-      <h5 className="mx-3.5 font-semibold text-xs">To Pay :</h5>
-      <h5 className="mx-10 font-semibold text-sm"><span>&#8377;</span>240.00</h5>
-      </div>
+    <div className="bg-white mx-5 mb-4 h-60 px-0 pt-3 items-center  rounded-md shadow-lg text-blue-dark">
+    <div className="flex flex-col">
+    <h4 className="mx-3 font-semibold text-green-mineral text-xs">Bill Details</h4>
+    <hr className="bg-silver  mx-3 my-3" />
+    <div className="flex flex-row justify-between">
+    <h5 className="ml-3.5 font-semibold text-xs">Item Total :</h5>
+    <h5 className="mx-10 font-semibold text-sm"><span>&#8377;</span>{totalPrice}</h5>
     </div>
-      </div>
+    <hr className="bg-silver  mx-3 my-3" />
+    <div className="flex flex-col">
+          {taxList.map((tax, index) => (
+            <div key={index} className="flex flex-row py-2.5 justify-between">
+              <div className="mx-3.5 flex items-center">
+                <span className="font-normal text-xs">{tax.key} :</span>
+              </div>
+              <h5 className="mx-10 font-semibold text-xs text-gray-800 text-opacity-80"><span>&#8377;</span>{tax.value}</h5>
+            </div>
+          ))}
+        </div>
+    <hr className="bg-silver  mx-3 my-3" />
+    <div className="flex flex-row justify-between">
+    <h5 className="mx-3.5 font-semibold text-xs">To Pay :</h5>
+    <h5 className="font-semibold text-sm text-grey mx-10"><span>&#8377;</span>{totalTax}</h5>
+    </div>
+  </div>
+    </div>
   )
 }
 
--- src/components/TaxCharges/TaxCharges.tsx
+++ src/components/TaxCharges/TaxCharges.tsx
@@ -1,57 +1,44 @@
-import React from 'react'
-import {StripeComponent} from '../ContainerCart/StripeComponent'
+import React from 'react';
+import { StripeComponent } from '../ContainerCart/StripeComponent';
 
 interface TaxChargesProps {
   totalPrice: number;
   totalTax: number;
-  cgst: number;
-  sgst: number;
-  serviceCharge:number;
+  taxList: { key: string; value: any; }[];
 }
 
-export const TaxCharges: React.FC<TaxChargesProps> = ({ totalPrice, totalTax, cgst, sgst,serviceCharge = 0  }) => {
+export const TaxCharges: React.FC<TaxChargesProps> = ({ totalPrice, totalTax, taxList }) => {
   return (
-   <>
-    <StripeComponent title="Bill Details"/>
+    <>
+      <StripeComponent title="Bill Details" />
       <div className="bg-white text-blue-dark px-3.5 py-4 mb-20">
-      <div className="flex flex-col">
-      <div className="flex flex-row justify-between">
-      <div className="flex items-center">
-        <span className="font-semibold text-xs">Item Total :</span>
+        <div className="flex flex-col">
+          <div className="flex flex-row justify-between">
+            <div className="flex items-center">
+              <span className="font-semibold text-xs">Item Total :</span>
+            </div>
+            <h5 className="font-semibold text-sm text-grey"><span>&#8377;</span>{totalPrice}</h5>
+          </div>
+          <hr className="bg-silver mx-4 my-3" />
+          <div className="flex flex-col">
+            {taxList.map((tax, index) => (
+              <div key={index} className="flex flex-row py-2.5 justify-between">
+                <div className="flex items-center">
+                  <span className="font-normal text-xs">{tax.key} :</span>
+                </div>
+                <h5 className="font-semibold text-xs text-gray-800 text-opacity-80"><span>&#8377;</span>{tax.value}</h5>
+              </div>
+            ))}
+          </div>
+          <hr className="bg-silver mx-4 my-3" />
+          <div className="flex flex-row justify-between">
+            <div className="flex items-center">
+              <span className="font-semibold text-xs">To Pay :</span>
+            </div>
+            <h5 className="font-semibold text-sm text-grey"><span>&#8377;</span>{totalTax}</h5>
+          </div>
+        </div>
       </div>
-      <h5 className="font-semibold text-sm text-grey"><span>&#8377;</span>{totalPrice}</h5>
-      </div>
-      <hr className="bg-silver  mx-4 my-3" />
-      <div className="flex flex-col">
-      {serviceCharge !== 0 && <div className="flex flex-row justify-between">
-      <div className="flex items-center">
-        <span className="font-normal text-xs">Service Charge :</span>
-      </div>
-      <h5 className="font-semibold text-xs text-gray-800 text-opacity-80"><span>&#8377;</span>30.00</h5>
-      </div>}
-      <div className="flex flex-row py-2.5 justify-between">
-      <div className="flex items-center">
-        <span className="font-normal text-xs">CGST :</span>
-      </div>
-      <h5 className="font-semibold text-xs text-gray-800 text-opacity-80"><span>&#8377;</span>{cgst}</h5>
-      </div>
-      <div className="flex flex-row justify-between">
-      <div className="flex items-center">
-        <span className="font-normal text-xs">SGST :</span>
-      </div>
-      <h5 className="font-semibold text-xs text-gray-800 text-opacity-80"><span>&#8377;</span>{sgst}</h5>
-      </div>
-      </div>
-      <hr className="bg-silver  mx-4 my-3" />
-      <div className="flex flex-row justify-between">
-      <div className="flex items-center">
-        <span className="font-semibold text-xs">To Pay :</span>
-      </div>
-      <h5 className="font-semibold text-sm text-grey"><span>&#8377;</span>{totalPrice+totalTax}</h5>
-      </div>
-    </div>
-      </div>
-   </>
+    </>
   )
 }
-
