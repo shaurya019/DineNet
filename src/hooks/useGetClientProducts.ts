ff4a928530687f96f6857eb7540c7808284436ec
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-04-01 13:00:10 +0530

    tags

--- src/hooks/useGetClientProducts.ts
+++ src/hooks/useGetClientProducts.ts
@@ -1,9 +1,9 @@
 import Alpine from "@/service/alpine";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetClientProducts = (client_id: string) => {
+export const useGetClientProducts = (client_id: string,totalTags?:any) => {
   return useQuery({
-    queryKey: ["useGetClientProducts", client_id],
+    queryKey: ["useGetClientProducts", client_id,totalTags],
     queryFn: () => Alpine.getClientProducts(client_id),
   });
 };
