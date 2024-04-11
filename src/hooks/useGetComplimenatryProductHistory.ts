80e90cd7a76090a4df6493567fcfba1599aaca1b
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-04-11 12:51:09 +0530

    fix the null request call

--- src/hooks/useGetComplimenatryProductHistory.ts
+++ src/hooks/useGetComplimenatryProductHistory.ts
@@ -1,9 +1,13 @@
 import Alpine from "@/service/alpine";
 import { useQuery } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
+import { RootState } from "@/service/store/cartStore";
 
 export const useGetComplimenatryProductHistory = (page:any) => {
+  const { loggedIn } = useSelector((state: RootState) => state.user);
   return useQuery({
     queryKey: ["useGetOrderHistory",page ],
     queryFn: () => Alpine.getComplimenatryProductHistory(page),
+    enabled: !!loggedIn,
   });
 };
--- src/hooks/useGetOrderHistory.ts
+++ src/hooks/useGetOrderHistory.ts
@@ -1,9 +1,13 @@
 import Alpine from "@/service/alpine";
 import { useQuery } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
+import { RootState } from "@/service/store/cartStore";
 
 export const useGetOrderHistory = (page:any) => {
+  const { loggedIn } = useSelector((state: RootState) => state.user);
   return useQuery({
     queryKey: ["useGetOrderHistory", page],
     queryFn: () => Alpine.getOrderHistory(page),
+    enabled: !!loggedIn,
   });
 };
