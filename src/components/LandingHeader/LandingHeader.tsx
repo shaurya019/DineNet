2134105d50a4caadeb93aa0e4ec490f6747cc791
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-04-14 13:27:15 +0530

    fix the name

--- src/components/LandingHeader/LandingHeader.tsx
+++ src/components/LandingHeader/LandingHeader.tsx
@@ -37,7 +37,7 @@ export const LandingHeader = ({clientName}:LandingHeaderProps) => {
         <img src="/assets/logo.png" />
       </div>
       <div className="flex-1 flex flex-col">
-        <h3 className="text-grey-dark font-bold">Welcome to the {clientName}!</h3>
+        <h3 className="text-grey-dark font-bold">Welcome to the {clientName}</h3>
         <p className="text-grey-dark text-xs">
           You have checked in into room no. {source}
         </p>
