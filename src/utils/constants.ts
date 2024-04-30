437260cf89680a32f06d895a415f18b0c0f65cbc
Piyush Sinha <piyushsinha2309@gmail.com>
2024-04-30 11:31:58 +0530

    Updated default source

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const defaultOpenTime = "0000";
+export const defaultCloseTime = "2359";
+export const defaultClientId = window.localStorage.getItem("clientId") || "1";
+export const defaultSource = window.localStorage.getItem("source") || "Room 1";
+export const BASE_URL = "https://staging-api.cubik.in/api/v1";
