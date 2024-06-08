d4b6dfdf915d7d8e565ee89969d7673f3e4b4cf9
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-06-08 11:27:16 +0530

    fix the customization issue

--- src/hooks/usePostOrderDetails.ts
+++ src/hooks/usePostOrderDetails.ts
@@ -4,12 +4,12 @@ import { useSelector } from "react-redux";
 import { defaultClientId, defaultSource } from '@/utils/constants';
 import { RootState } from "@/service/store/cartStore";
 
-export const usePostOrderDetails = (name: any,phone:any,instruction: any, firebaseToken: any,ChooseOption: any) => {
+export const usePostOrderDetails = (name: any, phone: any, instruction: any, firebaseToken: any, ChooseOption: any) => {
   const clientId = window.localStorage.getItem("clientId") || defaultClientId;
   const source = window.localStorage.getItem("source") || defaultSource;
   const items = useSelector((state: RootState) => state.cart.carts[clientId]?.[source]?.items);
   return useMutation({
     mutationKey: ["usePostOrder",],
-    mutationFn: () => Alpine.postOrderDetails(name,phone,ChooseOption, instruction, firebaseToken, items, clientId, source),
+    mutationFn: () => Alpine.postOrderDetails(name,phone,instruction, firebaseToken,ChooseOption, items, clientId, source),
   });
 };
